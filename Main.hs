260b0b29e945100d8bde949ed712283eca0ea86b
Josh Miller <notjoshmiller@gmail.com>
2020-12-01 23:32:53 -0600

    Add doctests

--- Main.hs
+++ Main.hs
@@ -44,9 +44,21 @@ asInts = fmap read
 day1a :: IO (Maybe Int)
 day1a = load "day1.txt" <&> asInts <&> solveDay1a
 
+day1Example :: [Int]
+day1Example = [1721, 979, 366, 299, 675, 1456]
+
+-- | Solve Day One Part One
+-- >>> solveDay1a day1Example
+-- Just 514579
 solveDay1a :: [Int] -> Maybe Int
 solveDay1a ints = M.listToMaybe solution
   where
+    -- For each suffix of the list, keep a reference to the head (x) and iterate over the tail
+    -- For each element of the suffix's tail, sum it with the head (x)
+    -- The tails law ensures the first element is the original list: head (tails xs) = xs
+    -- My original approach used the cartesian product of the list with itself,
+    -- which produces duplicates and mirrored pairs.
+    -- While that does work it is wasteful, tails lets us compute only what we need.
     solution = do
       x:xs <- L.tails ints
       y    <- xs
@@ -69,6 +81,9 @@ solveDay1a ints = M.listToMaybe solution
 day1b :: IO (Maybe Int)
 day1b = load "day1.txt" <&> asInts <&> solveDay1b
 
+-- | Solve Day One Part Two
+-- >>> solveDay1b day1Example
+-- Just 241861950
 solveDay1b :: [Int] -> Maybe Int
 solveDay1b ints = M.listToMaybe solution
   where
