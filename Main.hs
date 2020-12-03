c0975338b5c1f5402945aaf89a524f60cfa74f23
Josh Miller <notjoshmiller@gmail.com>
2020-12-03 01:27:05 -0600

    Add Day 3 solutions

--- Main.hs
+++ Main.hs
@@ -4,14 +4,16 @@
 module Main where
 
 import           Control.Monad (guard)
+import           Data.Bifunctor (first)
 import           Data.Char (digitToInt)
 import           Data.Functor  ((<&>))
 import           Data.Function ((&))
-import           Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
 import qualified Data.List as L
 import qualified Data.List.Split as L
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 import qualified Data.Maybe as M
+import           Data.Traversable (for)
 
 main :: IO ()
 main = do
@@ -19,6 +21,8 @@ main = do
   print =<< day1b
   print =<< day2a
   print =<< day2b
+  print =<< day3a
+  print =<< day3b
 
 load :: FilePath -> IO [String]
 load = fmap lines . readFile . ("text/" <>)
@@ -26,9 +30,9 @@ load = fmap lines . readFile . ("text/" <>)
 asInts :: [String] -> [Int]
 asInts = fmap read
 
--- ===============================
--- Day 1: Report Repair
--- ===============================
+-- =======================
+-- Day 1: Report Repair --
+-- =======================
 
 -- Before you leave, the Elves in accounting just need you to fix your expense report
 -- (your puzzle input); apparently, something isn't quite adding up.
@@ -74,9 +78,9 @@ solveDay1a ints = M.listToMaybe solution
       guard $ x + y == 2020
       pure  $ x * y
 
--- ===============================
--- Day 1 Part Two
--- ===============================
+-- =================
+-- Day 1 Part Two --
+-- =================
 -- The Elves in accounting are thankful for your help; one of them even
 -- offers you a starfish coin they had left over from a past vacation.
 -- They offer you a second one if you can find three numbers in your
@@ -103,9 +107,9 @@ solveDay1b ints = M.listToMaybe solution
       guard $ x + y + z == 2020
       pure  $ x * y * z
 
--- ===============================
--- Day 2: Password Philosophy
--- ===============================
+-- =============================
+-- Day 2: Password Philosophy --
+-- =============================
 -- Your flight departs in a few days from the coastal airport; the easiest way
 -- down to the coast from here is via toboggan.
 
@@ -174,9 +178,9 @@ getValidPassword Policy{pVar1=pMin,pVar2=pMax,pCh} pass = do
 solveDay2a :: [(Policy, Password)] -> Int
 solveDay2a = length . M.mapMaybe (uncurry getValidPassword)
 
--- ===============================
--- Day 2 Part Two
--- ===============================
+-- =================
+-- Day 2 Part Two --
+-- =================
 -- While it appears you validated the passwords correctly, they don't seem to be what
 -- the Official Toboggan Corporate Authentication System is expecting.
 
@@ -212,3 +216,135 @@ getNewValidPassword Policy{pVar1=pPos1,pVar2=pPos2,pCh} pass = do
   guard $ length (filter ((||) <$> (pPos1 ==) <*> (pPos2 ==)) indices) == 1
   pure pass
 
+-- =============================
+-- Day 3: Toboggan Trajectory --
+-- =============================
+-- With the toboggan login problems resolved, you set off toward the airport.
+-- While travel by toboggan might be easy, it's certainly not safe:
+-- there's very minimal steering and the area is covered in trees.
+-- You'll need to see which angles will take you near the fewest trees.
+
+-- Due to the local geology, trees in this area only grow on exact
+-- integer coordinates in a grid. You make a map (your puzzle input)
+-- of the open squares (.) and trees (#) you can see. For example:
+
+-- ..##.......
+-- #...#...#..
+-- .#....#..#.
+-- ..#.#...#.#
+-- .#...##..#.
+-- ..#.##.....
+-- .#.#.#....#
+-- .#........#
+-- #.##...#...
+-- #...##....#
+-- .#..#...#.#
+-- These aren't the only trees, though; due to something you read about once
+-- involving arboreal genetics and biome stability,
+-- the same pattern repeats to the right many times:
+
+-- ..##.........##.........##.........##.........##.........##.......  --->
+-- #...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+-- .#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+-- ..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+-- .#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+-- ..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+-- .#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+-- .#........#.#........#.#........#.#........#.#........#.#........#
+-- #.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+-- #...##....##...##....##...##....##...##....##...##....##...##....#
+-- .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+-- You start on the open square (.) in the top-left corner and need to reach the
+-- bottom (below the bottom-most row on your map).
+
+-- The toboggan can only follow a few specific slopes (you opted for a
+-- cheaper model that prefers rational numbers); start by counting all the trees you
+-- would encounter for the slope right 3, down 1:
+
+-- From your starting position at the top-left, check the position that
+-- is right 3 and down 1. Then, check the position that is right 3 and
+-- down 1 from there, and so on until you go past the bottom of the map.
+
+-- The locations you'd check in the above example are marked here with
+-- O where there was an open square and X where there was a tree:
+
+-- ..##.........##.........##.........##.........##.........##.......  --->
+-- #..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+-- .#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+-- ..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+-- .#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+-- ..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+-- .#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+-- .#........#.#........X.#........#.#........#.#........#.#........#
+-- #.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+-- #...##....##...##....##...#X....##...##....##...##....##...##....#
+-- .#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+-- In this example, traversing the map using this slope would cause you to encounter 7 trees.
+
+-- Starting at the top-left corner of your map and following a slope of right 3 and down 1,
+-- how many trees would you encounter?
+
+day3a :: IO Int
+day3a = load "day3.txt" <&> solveDay3a 3 1
+
+day3Example :: [String]
+day3Example =
+  [ "..##......."
+  , "#...#...#.."
+  , ".#....#..#."
+  , "..#.#...#.#"
+  , ".#...##..#."
+  , "..#.##....."
+  , ".#.#.#....#"
+  , ".#........#"
+  , "#.##...#..."
+  , "#...##....#"
+  , ".#..#...#.#"
+  ]
+
+-- | Solve Day 3 Part One
+-- >>> solveDay3a 3 1 day3Example
+-- 7
+solveDay3a :: Int -> Int -> [String] -> Int
+solveDay3a sx sy ls = go 0 0 0
+  where
+    go x y acc
+      | y >= length ls = acc
+      | otherwise =
+        case (fmap (concat . repeat) ls !! y) !! x of
+          '#' -> go (x + sx) (y + sy) (acc + 1)
+          '.' -> go (x + sx) (y + sy) acc
+          _   -> error "Invalid map character"
+
+-- =================
+-- Day 3 Part Two --
+-- =================
+-- Time to check the rest of the slopes - you need to minimize the
+-- probability of a sudden arboreal stop, after all.
+
+-- Determine the number of trees you would encounter if,
+-- for each of the following slopes, you start at the top-left corner
+-- and traverse the map all the way to the bottom:
+
+-- Right 1, down 1.
+-- Right 3, down 1. (This is the slope you already checked.)
+-- Right 5, down 1.
+-- Right 7, down 1.
+-- Right 1, down 2.
+-- In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively;
+-- multiplied together, these produce the answer 336.
+
+-- What do you get if you multiply together the number of trees
+-- encountered on each of the listed slopes?
+
+day3b :: IO Int
+day3b = load "day3.txt" <&> solveDay3b
+
+slopes :: [(Int, Int)]
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+-- | Solve Day 3 Part One
+-- >>> solveDay3b day3Example
+-- 336
+solveDay3b :: [String] -> Int
+solveDay3b ls = product $ uncurry (flip flip ls . solveDay3a) <$> slopes
